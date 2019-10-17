edee833c70d41e5e452850ec18fd842c899b3429
ZanderPotgieter <u18214496@tuks.co.za>
2019-10-17 23:38:49 +0200

    Add project files.

---
+++ INF272_F4Y/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="INF272_F4Y.MvcApplication" Language="C#" %>
---
+++ INF272_F4Y/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(INF272_F4Y.Startup))]
+namespace INF272_F4Y
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
